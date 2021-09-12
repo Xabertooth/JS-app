57a1b29c7ba86f7184753181a6a345a8a23a4080
Xabertooth <konigan@gmail.com>
2021-09-12 22:12:58 +0500

    random 3

--- 3.1-random.js
+++ 3.1-random.js
@@ -1,7 +1,8 @@
 'use strict'
 
 function random(min, max){
+    if ((max<min) || (max === undefined || 0)) max = min+1;
     return Math.floor(Math.random()*((max-min)+min));
 }
 
-console.log(random(5,178));
+console.log(random(5,undefined));
